95f4f2257137ec412cc70d36c769bac34f49aaab
Oleksii Shcherbak <qksardas001@mail.com>
2025-10-27 10:58:59 +0200

    fixing bugs

--- app/main.py
+++ app/main.py
@@ -1,12 +1,3 @@
-class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int,
-                 brand: str) -> None:
-        self.comfort_class = comfort_class
-        self.clean_mark = clean_mark
-        self.brand = brand
-
-
 class CarWashStation:
     def __init__(self, distance_from_city_center: float,
                  clean_power: int,
@@ -23,20 +14,23 @@ class CarWashStation:
                  * self.average_rating / self.distance_from_city_center)
         return round(price, 1)
 
-    def wash_single_car(self, car: Car) -> float:
+    def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
-            price = self.calculate_washing_price(car)
             car.clean_mark = self.clean_power
-            return price
-        return 0.0
 
     def serve_cars(self, cars: list[Car]) -> float:
         income = 0.0
         for car in cars:
-            income += self.wash_single_car(car)
+            if car.clean_mark < self.clean_power:
+                price = self.calculate_washing_price(car)
+                income += price
+                self.wash_single_car(car)
         return round(income, 1)
 
-    def rate_service(self, new_rate: int) -> None:
+    def rate_service(self, new_rate: int) -> float:
+        if new_rate < 0:
+            new_rate = 0
         total_rating = self.average_rating * self.count_of_ratings + new_rate
         self.count_of_ratings += 1
         self.average_rating = round(total_rating / self.count_of_ratings, 1)
+        return self.average_rating
